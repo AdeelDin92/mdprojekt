34fe5a0a33ca07cced33ec77de30e70ad267329c
ADEEL <AdeelDin@outlook.de>
2022-06-22 22:40:48 +0200

    Build version

---
+++ build/precache-manifest.6f1a35d76f69116f7f8be57d1f385f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b147a7071207f80f6fdf4bde917720e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7f53069d0b091a6d502",
+    "url": "/static/css/2.c1c45ef1.chunk.css"
+  },
+  {
+    "revision": "776dc5d417350a9ec5c5",
+    "url": "/static/css/main.1dabb3ef.chunk.css"
+  },
+  {
+    "revision": "a7f53069d0b091a6d502",
+    "url": "/static/js/2.d3437582.chunk.js"
+  },
+  {
+    "revision": "776dc5d417350a9ec5c5",
+    "url": "/static/js/main.ccac0bbf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
