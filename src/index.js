f27282417af9c6001e424e1094508096cdf29b90
ADEEL <AdeelDin@outlook.de>
2022-06-22 23:09:18 +0200

    React to 18 updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-
 import App from "./components/App";
 
-ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
